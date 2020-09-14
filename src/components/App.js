7e7cf2071104dab8472d9f680f691b846248585f
Badbola <divyanshuok@outlook.com>
2020-09-14 21:32:56 +0530

    Added router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { SignIn } from "./";
 
-function App() {
-  return <div className="App">Hello world</div>;
+function change() {
+  return <div>Changed</div>;
+}
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/changed" component={change} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 import App from "./App";
-
-export { App };
+import SignIn from "./SignIn";
+export { App, SignIn };
